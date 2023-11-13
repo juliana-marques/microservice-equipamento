4a580251d1c94a2266391b548ae34e382819aee2
Juliana Marques <julianamarquesdsc@gmail.com>
2023-11-12 23:13:25 -0300

    push totem_crud

--- src/controller/main.py
+++ src/controller/main.py
@@ -6,7 +6,8 @@ project_root = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
 sys.path.insert(0, project_root)
 
 from service.BicicletaService import listar_bicicletas, cadastrar_bicicleta, editar_bicicleta, validar_id, deletar_bicicleta
-from service.TrancaService import listar_trancas
+from service.TotemService import listar_totens, cadastrar_totem, editar_totem, validar_id_totem, deletar_totem
+from service.TrancaService import listar_trancas, cadastrar_tranca
 
 
 app = Flask(__name__)
@@ -57,6 +58,44 @@ def deletar_bicicleta_route(bicicleta_id):
     response = deletar_bicicleta(bicicleta_id)
     return response
 
+@app.route('/totem', methods=['GET'])
+def listar_totens_route():
+    totens = listar_totens()
+    return totens
+
+@app.route('/totem', methods=['POST'])
+def cadastrar_totem_route():
+    data = request.form
+
+    localizacao = data.get('localizacao')
+    descricao = data.get('descricao')
+
+    response = cadastrar_totem(localizacao, descricao)
+
+    return response
+
+@app.route('/totem/<int:idTotem>', methods=['PUT'])
+def editar_totem_route(idTotem):
+
+    verificacao = validar_id_totem(idTotem)
+
+    if verificacao != True:
+        return verificacao
+
+    data = request.form
+
+    localizacao = data.get('localizacao')
+    descricao = data.get('descricao')
+
+    response = editar_totem(idTotem,localizacao, descricao)
+
+    return response
+
+@app.route('/totem/<int:idTotem>', methods=['DELETE'])
+def deletar_totem_route(idTotem):
+
+    response = deletar_totem(idTotem)
+    return response
 
 @app.route('/tranca', methods=['GET'])
 def obter_trancas_route():
@@ -64,6 +103,19 @@ def obter_trancas_route():
     trancas = listar_trancas()
     return trancas
 
+@app.route('/tranca', methods=['POST'])
+def cadastrar_trancas_route():
+    data = request.get_json()
+
+    numero = data.get('numero')
+    localizacao = data.get('localizacao')
+    anoDeFabricacao = data.get('anoDeFabricacao')
+    modelo = data.get('modelo')
+    status = data.get('status')
+
+    response = cadastrar_tranca(numero, localizacao, anoDeFabricacao, modelo, status)
+
+    return response
 
 if __name__ == '__main__':
     app.run(port=int(os.environ.get("PORT", 4000)),host='0.0.0.0',debug=True)
