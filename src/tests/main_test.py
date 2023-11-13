492ffb5ca1c8ea3909ce0b45e2be5932653d0d70
Juliana Marques <julianamarquesdsc@gmail.com>
2023-11-13 11:20:29 -0300

    Update main_test.py

--- src/tests/main_test.py
+++ src/tests/main_test.py
@@ -9,7 +9,7 @@ from service.BicicletaService import listar_bicicletas, cadastrar_bicicleta, edi
 
 class TestMain(unittest.TestCase):
 
-    @patch('main.listar_bicicletas')  # Mock a função listar_bicicletas
+    @patch('main.listar_bicicletas')
     def test_listar_bicicletas_route(self, mock_listar_bicicletas):
 
         mock_listar_bicicletas.return_value = "Bicicletas listadas"
