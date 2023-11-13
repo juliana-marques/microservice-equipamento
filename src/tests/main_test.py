2769a9e6a1ce0febf85f64920230d97b1b2c535a
Juliana Marques <julianamarquesdsc@gmail.com>
2023-11-13 20:37:32 -0300

    add test main totem

--- src/tests/main_test.py
+++ src/tests/main_test.py
@@ -5,7 +5,6 @@ project_root = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
 sys.path.insert(0, project_root)
 
 from controller.main import app
-from service.BicicletaService import listar_bicicletas, cadastrar_bicicleta, editar_bicicleta, validar_id, deletar_bicicleta
 
 class TestMain(unittest.TestCase):
 
@@ -64,6 +63,56 @@ class TestMain(unittest.TestCase):
             response = client.delete('/bicicleta/2')
 
             self.assertEqual(response.status_code, mock_deletar_bicicletas.status_code)
+
+    @patch('controller.main.Mock')
+    def test_listar_totem_route(self, mock_listar_totem):
+
+        mock_listar_totem.status_code = 200
+
+        with app.test_client() as client:
+            response = client.get('/totem')
+
+            self.assertEqual(response.status_code, mock_listar_totem.status_code)
+
+    @patch('controller.main.Mock')
+    def test_cadastrar_totem_route(self, mock_cadastrar_totem):
+
+        mock_cadastrar_totem.status_code = 200
+
+        data = {
+            "id": 1,
+            "localizacao": "teste",
+            "descricao": "teste"
+        }
+
+        with app.test_client() as client:
+            response = client.post('/totem', json=data)
+            self.assertEqual(response.status_code, mock_cadastrar_totem.status_code)
+
+    @patch('controller.main.Mock')
+    def test_editar_totem_route(self, mock_editar_totem):
+
+        mock_editar_totem.status_code = 200
+
+        data = {
+            "id": 1,
+            "localizacao": "teste",
+            "descricao": "teste"
+        }
+
+        with app.test_client() as client:
+            response = client.put('/totem/1', json=data)
+            self.assertEqual(response.status_code, mock_editar_totem.status_code)
+
+    @patch('controller.main.Mock')
+    def test_deletar_totem_route(self, mock_deletar_totem):
+
+        mock_deletar_totem.status_code = 200
+
+        with app.test_client() as client:
+            response = client.delete('/totem1')
+
+            self.assertEqual(response.status_code, mock_deletar_totem.status_code)
 
 
 if __name__ == '__main__':
