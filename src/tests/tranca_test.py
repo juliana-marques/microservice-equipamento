907309667714ee361df9474b44b1bd518ff7c1c8
Juliana Marques <julianamarquesdsc@gmail.com>
2023-11-13 10:22:30 -0300

    fix tranca test

--- src/tests/tranca_test.py
+++ src/tests/tranca_test.py
@@ -6,21 +6,12 @@ sys.path.insert(0, project_root)
 
 from service.TrancaService import listar_trancas, cadastrar_tranca, buscar_tranca_por_id, editar_tranca, deletar_tranca, validar_id
 
-from your_module import (
-    listar_trancas,
-    cadastrar_tranca,
-    buscar_tranca_por_id,
-    editar_tranca,
-    deletar_tranca,
-    validar_id
-)
-
 modelo_a = "Modelo A"
 disponivel = "DISPONIVEL"
 
 class TestAppFunctions(unittest.TestCase):
 
-    @patch('your_module.Mock')
+    @patch('service.TrancaService.Mock')
     def test_listar_trancas(self, mock_response):
         mock_response.status_code = 200
         mock_response.json.return_value = [
@@ -45,12 +36,12 @@ class TestAppFunctions(unittest.TestCase):
             
         ]
 
-        with patch('your_module.listar_trancas', return_value=mock_response.json()):
+        with patch(listar_trancas, return_value=mock_response.json()):
             result = listar_trancas()
 
         self.assertEqual(result, mock_response.json())
 
-    @patch('your_module.Mock')
+    @patch('service.TrancaService.Mock')
     def test_cadastrar_tranca(self, mock_response):
         numero = 1
         localizacao = "Botafogo"
@@ -67,12 +58,12 @@ class TestAppFunctions(unittest.TestCase):
             "status": status
         }
 
-        with patch('your_module.cadastrar_tranca', return_value=mock_response.json()):
+        with patch(cadastrar_tranca, return_value=mock_response.json()):
             result = cadastrar_tranca(numero, localizacao, ano_de_fabricacao, modelo, status)
 
         self.assertEqual(result, mock_response.json())
 
-    @patch('your_module.Mock')
+    @patch('service.TrancaService.Mock')
     def test_buscar_tranca_por_id(self, mock_response):
         # Mocking the ID for testing
         id_tranca = 1
@@ -89,18 +80,17 @@ class TestAppFunctions(unittest.TestCase):
             "status": disponivel
         }
 
-        with patch('your_module.buscar_tranca_por_id', return_value=mock_response.json()):
+        with patch(buscar_tranca_por_id, return_value=mock_response.json()):
             result = buscar_tranca_por_id(id_tranca)
 
         self.assertEqual(result, mock_response.json())
 
-    # Add tests for other functions like editar_tranca, deletar_tranca, and validar_id here
-    @patch('your_module.Mock')
+
+    @patch('service.TrancaService.Mock')
     def test_editar_tranca(self, mock_response):
-        # Mocking the ID for testing
+
         id_tranca = 1
 
-        # Mocking the input data
         data = {
             "numero": 1,
             "localizacao": "Copacabana",
@@ -109,39 +99,34 @@ class TestAppFunctions(unittest.TestCase):
             "status": "Ocupada"
         }
 
-        # Mocking the response from your service function
         mock_response.status_code = "Dados atualizados", 200
         mock_response.json.return_value = data
 
-        with patch('your_module.editar_tranca', return_value=mock_response.json()):
+        with patch(editar_tranca, return_value=mock_response.json()):
             result = editar_tranca(data, id_tranca)
 
         self.assertEqual(result, mock_response.json())
 
-    @patch('your_module.Mock')
+    @patch('service.TrancaService.Mock')
     def test_deletar_tranca(self, mock_response):
-        # Mocking the ID for testing
         id_tranca = 1
 
-        # Mocking the response from your service function
         mock_response.status_code = 200
         mock_response.json.return_value = "Tranca removida"
 
-        with patch('your_module.deletar_tranca', return_value=mock_response.json()):
+        with patch(deletar_tranca, return_value=mock_response.json()):
             result = deletar_tranca(id_tranca)
 
         self.assertEqual(result, mock_response.json())
 
-    @patch('your_module.Mock')
+    @patch('service.TrancaService.Mock')
     def test_validar_id(self, mock_response):
-        # Mocking the ID for testing
         id_tranca = 1
 
-        # Mocking the response from your service function
         mock_response.status_code = 200
         mock_response.json.return_value = True
 
-        with patch('your_module.validar_id', return_value=mock_response.json()):
+        with patch(validar_id, return_value=mock_response.json()):
             result = validar_id(id_tranca)
 
         self.assertEqual(result, mock_response.json())
