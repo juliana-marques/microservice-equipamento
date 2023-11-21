795a05d1ccb008a2c5f2e93e5368cdaf062ffb6e
Juliana Marques <julianamarquesdsc@gmail.com>
2023-11-21 09:20:48 -0300

    add get tranca + totem controller test

--- src/tests/test.py
+++ src/tests/test.py
@@ -5,6 +5,8 @@ sys.path.insert(0, project_root)
 
 from controller.main import app
 from repository.bicicleta_repository import BicletaRepository
+from repository.totem_repository import TotemRepository
+from repository.tranca_repository import TrancaRepository
 
 class TestRoutes(unittest.TestCase):
     def setUp(self):
@@ -19,6 +21,24 @@ class TestRoutes(unittest.TestCase):
 
         bicicletas_esperadas = BicletaRepository().listar_bicicleta()
         self.assertEqual(data, bicicletas_esperadas)
+
+    def test_listar_totens_route(self):
+        response = self.client.get('/totem')
+        data = response.get_json()
+
+        self.assertEqual(response.status_code, 200)
+
+        totens_esperados = TotemRepository().listar_totens()
+        self.assertEqual(data, totens_esperados)
+
+    def test_listar_trancas_route(self):
+        response = self.client.get('/tranca')
+        data = response.get_json()
+
+        self.assertEqual(response.status_code, 200)
+
+        trancas_esperaas = TrancaRepository().listar_trancas()
+        self.assertEqual(data, trancas_esperaas)
 
 if __name__ == '__main__':
     unittest.main()
