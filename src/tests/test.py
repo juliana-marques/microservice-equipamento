f0f7db73bd3fed3c8f084b589a61371735b1a9bd
Juliana Marques <julianamarquesdsc@gmail.com>
2023-11-21 11:55:27 -0300

    add get por id tests

--- src/tests/test.py
+++ src/tests/test.py
@@ -5,6 +5,9 @@ project_root = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
 sys.path.insert(0, project_root)
 
 from controller.main import app
+from service.BicicletaService import listar_bicicleta_id
+from service.TotemService import listar_totem_id
+from service.TrancaService import listar_tranca_id
 from repository.bicicleta_repository import BicletaRepository
 from repository.totem_repository import TotemRepository
 from repository.tranca_repository import TrancaRepository
@@ -24,6 +27,15 @@ class TestRoutes(unittest.TestCase):
         bicicletas_esperadas = BicletaRepository().listar_bicicleta()
         self.assertEqual(data, bicicletas_esperadas)
 
+
+    def test_listar_bicicleta_id_route(self):
+        response = self.client.get('/bicicleta/1')
+        data = response.text
+
+        bicicletas_esperadas = listar_bicicleta_id(1)
+        self.assertEqual(data, bicicletas_esperadas)
+
+
     def test_listar_totens_route(self):
         response = self.client.get('/totem')
         data = response.get_json()
@@ -31,6 +43,14 @@ class TestRoutes(unittest.TestCase):
         self.assertEqual(response.status_code, 200)
 
         totens_esperados = TotemRepository().listar_totens()
+        self.assertEqual(data, totens_esperados)
+
+
+    def test_listar_totens_id_route(self):
+        response = self.client.get('/totem/1')
+        data = response.text
+
+        totens_esperados = listar_totem_id(1)
         self.assertEqual(data, totens_esperados)
 
     def test_listar_trancas_route(self):
@@ -41,6 +61,14 @@ class TestRoutes(unittest.TestCase):
 
         trancas_esperaas = TrancaRepository().listar_trancas()
         self.assertEqual(data, trancas_esperaas)
+
+
+    def test_listar_tranca_id_route(self):
+        response = self.client.get('/tranca/1')
+        data = response.text
+
+        trancas_esperados = listar_tranca_id(1)
+        self.assertEqual(data, trancas_esperados)
 
     
     ################################################################################
