aeeb9acd700ca6c3d5b311a676890f0b9a7bf273
Juliana Marques <julianamarquesdsc@gmail.com>
2023-11-21 11:43:48 -0300

    delete model

--- src/model/totem.py
+++
@@ -1,8 +0,0 @@
-import os, sys
-project_root = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))                    
-sys.path.insert(0, project_root) 
-
-class Totem:
-    def __init__(self, localizacao, descricao):
-        self.localizacao = localizacao
-        self.descricao = descricao
