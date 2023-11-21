51d58fb93d68963df4f8f27884434ae6b8a177a3
Juliana Marques <julianamarquesdsc@gmail.com>
2023-11-21 10:58:35 -0300

    add POST test bicicleta

--- src/tests/test.py
+++ src/tests/test.py
@@ -1,4 +1,5 @@
-import unittest, os, sys
+import unittest, os, sys, json
+from unittest.mock import Mock, patch
 
 project_root = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
 sys.path.insert(0, project_root)
@@ -7,6 +8,8 @@ from controller.main import app
 from repository.bicicleta_repository import BicletaRepository
 from repository.totem_repository import TotemRepository
 from repository.tranca_repository import TrancaRepository
+from service.BicicletaService import listar_bicicletas, cadastrar_bicicleta, editar_bicicleta, deletar_bicicleta, listar_bicicleta_id, integrar_bicicleta_rede, retirar_bicicleta_rede
+
 
 class TestRoutes(unittest.TestCase):
     def setUp(self):
@@ -39,6 +42,26 @@ class TestRoutes(unittest.TestCase):
 
         trancas_esperaas = TrancaRepository().listar_trancas()
         self.assertEqual(data, trancas_esperaas)
+
+    
+    ################################################################################
+    #                                                                              #
+    # o patch é usado para substituir a função chamada por um objeto de simulação  #
+    # o objeto passado por parâmetro é um objeto "mock" para isolar o teste        #
+    #                                                                              #
+    ################################################################################
+
+    @patch('service.BicicletaService.cadastrar_bicicleta')
+    def test_cadastrar_bicicleta_route(self, mock_cadastrar_bicicleta):
+        dados_cadastrados = {"id": "1", "marca": "marca_teste", "modelo": "modelo_teste", "ano": "2023", "numero": 1, "status": "DISPONIVEL"}
+        mock_cadastrar_bicicleta.return_value = dados_cadastrados
+
+        response = self.client.get('/get_csrf_token')
+        token = response.get_data(as_text=True)
+        response = self.client.post('/bicicleta', headers={"Content-Type": "application/json", "X-CSRFToken": token}, json=dados_cadastrados)
+
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.json, dados_cadastrados)
 
 if __name__ == '__main__':
     unittest.main()
